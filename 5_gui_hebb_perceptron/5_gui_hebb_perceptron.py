c03b9b6a5885c0fc745aa5bb8567ec50df879c19
DanGM96 <jordangm@outlook.com>
2020-12-14 05:50:55 -0300

    GUI Hebb e Perceptron usando neurônio

---
+++ 5_gui_hebb_perceptron/5_gui_hebb_perceptron.py
@@ -0,0 +1,113 @@
+from PyQt5 import QtCore, uic
+from PyQt5.QtWidgets import QApplication, QLabel, \
+    QPushButton, QSpinBox, QComboBox
+
+from hebb_rule import hebb_rule
+from perceptron import perceptron
+
+BIAS = 1 # Bias padrão utilizado nos algoritmos
+
+def get_choice():
+    """ Muda o nome da janela dinâmicamente """
+    window.setWindowTitle("GUI - " + choice.currentText())
+
+def get_sb(_object):
+    """ Retorna os objetos QSpinBox """
+    return window.findChild(QSpinBox, _object)
+
+def get_lbl(_object):
+    """ Retorna os objetos QLabel """
+    return window.findChild(QLabel, _object)
+
+def get_train_data():
+    """ Organiza as entradas do usuário """
+    train = []
+    # Set 1
+    train.append(get_sb("spinBox_in_1a").value()) # in a
+    train.append(get_sb("spinBox_in_1b").value()) # in b
+    train.append(get_sb("spinBox_out_1").value()) # out
+    # Set 2
+    train.append(get_sb("spinBox_in_2a").value()) # in a
+    train.append(get_sb("spinBox_in_2b").value()) # in b
+    train.append(get_sb("spinBox_out_2").value()) # out
+    # Set 3
+    train.append(get_sb("spinBox_in_3a").value()) # in a
+    train.append(get_sb("spinBox_in_3b").value()) # in b
+    train.append(get_sb("spinBox_out_3").value()) # out
+    # Set 4
+    train.append(get_sb("spinBox_in_4a").value()) # in a
+    train.append(get_sb("spinBox_in_4b").value()) # in b
+    train.append(get_sb("spinBox_out_4").value()) # out
+
+    # Retorna um vetor de ([entradas], saida)
+    return [([train[i], train[i + 1], BIAS], train[i + 2]) for i in range(0, len(train), 3)]
+
+def sub(_string):
+    """ Transforma string para substring utilizando html (aceito pelo Qt) """
+    return f"<span style=' vertical-align:sub;'>{_string}</span>"
+
+def start_trainning():
+    """ Faz o treinamento de acordo com os inputs e outputs e mostra os pesos """
+    data = get_train_data()
+    chosen = choice.currentText()
+
+    if chosen == "Hebb":
+        weight = hebb_rule(data)
+    elif chosen == "Perceptron":
+        weight = perceptron(data)
+    else:
+        weight = "err"
+
+    get_lbl("weightLabel_1").setText(f"W{sub('1')}: {weight[0]}")
+    get_lbl("weightLabel_2").setText(f"W{sub('2')}: {weight[1]}")
+    get_lbl("weightLabel_B").setText(f"W{sub('B')}: {weight[2]}")
+    store(weight)
+
+def process_weights(_input, _weight):
+    """ Processa os pesos de acordo com o neurônio e seu modelo matemático """
+    net = _weight[2] # Adiciona o bias primeiro
+    for i in range(len(_input)):
+        net += _input[i] * _weight[i] # Soma a multiplicação das entradas com os pesos
+
+    # Função de transferência - Degrau Unitário
+    if net <= 0:
+        return -1
+    return 1
+
+def store(*values):
+    """ Guarda valores de forma gambiarrística """
+    store.values = values or store.values
+    return store.values[0]
+
+def start_running():
+    """ Faz o teste de acordo com os inputs e mostra o resultado """
+    # Input
+    run_in = []
+    run_in.append(get_sb("spinBox_run_a").value())
+    run_in.append(get_sb("spinBox_run_b").value())
+
+    weight = store()
+    result = process_weights(run_in, weight)
+    # Output
+    get_lbl("outputLabel").setText(f"Output: {result}")
+
+if __name__ == "__main__":
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    app = QApplication([])
+    window = uic.loadUi(r"ia-cc-2020-2-jordan\5_gui_hebb_perceptron\hebb_perceptron.ui")
+    store([0, 0, 0])
+
+    # Escolha do algoritmo
+    choice = window.findChild(QComboBox, "choiceComboBox")
+    choice.activated.connect(get_choice)
+
+    # Botão do trainning
+    trainBtn = window.findChild(QPushButton, "trainButton")
+    trained = trainBtn.clicked.connect(start_trainning)
+
+    # Botão do running
+    runBtn = window.findChild(QPushButton, "runButton")
+    runBtn.clicked.connect(start_running)
+
+    window.show()
+    app.exec_()
---
+++ 5_gui_hebb_perceptron/hebb_rule.py
@@ -0,0 +1,24 @@
+# 8.3.1 - Regra de Hebb
+def zeros(n):
+    """ Cria um vetor de zeros """
+    return [0 for i in range(n)]
+
+def hebb_rule(dados):
+    """ Regra de Hebb para redes neurais """
+    # Passo 0: Inicializar todos os pesos
+    n = len(dados[0][0]) - 1
+    weight = zeros(n + 1)
+    #print(weight)
+
+    # Passo 1: Para cada vetor de treinamento na entrada e par de objetivos na saída (e : s)
+    for _, dado in enumerate(dados):
+        # Passo 2: Ajuste as ativações para as unidades de entrada
+        x = dado[0]
+        # Passo 3: Ajuste a ativação para a unidade de saída
+        y = dado[1]
+        # Passo 4: Ajuste os pesos e o bias
+        for j in range(n):
+            weight[j] += x[j] * y
+        weight[n] += + y # Bias é weight[n]
+        #print(weight)
+    return weight
