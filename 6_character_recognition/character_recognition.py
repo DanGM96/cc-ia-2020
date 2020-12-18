e0770c2cc809b0f622143691235c9a35b7a4230a
DanGM96 <jordangm@outlook.com>
2020-12-18 01:22:01 -0300

    RedeNeural completa

--- 6_character_recognition/character_recognition.py
+++ 6_character_recognition/character_recognition.py
@@ -1,10 +1,88 @@
-from gui import Gui
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication
+
+from gui import Gui
+from perceptron import Perceptron
+
+global gui
+gui = None
+
+class RedeNeural:
+
+    def __init__(self):
+        """ Constructor pré-gera a array de neurônios """
+        self.digitos = []
+        for i in range(0, 10):
+            self.digitos.append((Perceptron(), str(i)))
+
+    def train_one(self, ts_in, ts_index):
+        """ Usa o perceptron para aprender o reconhecimento de um dígito """
+        for index, digit in enumerate(self.digitos):
+            percep = digit[0]
+            ts = percep.training_set
+            if index != ts_index:
+                ts.append((ts_in, -1))
+            else:                
+                ts.append((ts_in, 1))
+            percep.update_parameters(ts, 0, 0, 1)
+            percep.train()
+
+    def train_all(self):
+        """ Treina os dígitos das fontes pré-existentes """
+        train_set = gui.font_a
+        for index, ts in enumerate(train_set):
+            self.train_one(ts, index)
+        self.fonte_b()
+
+    def train_current(self):
+        """ Treina o dígito na tela (criado pelo usuário) """
+        inputs = gui.inputs
+        selection = int(gui.character_cb.currentText())
+        self.train_one(inputs, selection)
+
+    def run(self):
+        """ Executa a rede neural e tenta reconhecer os números """
+        inputs = gui.inputs
+        #print(f"self.train_one({inputs}, {gui.character_cb.currentText()})")
+        output = "Recognized Numbers: "
+        for index, digit in enumerate(self.digitos):
+            net = digit[0].calculate_net(inputs)
+            if digit[0].calculate_output(net) == 1:
+                output += f"{index} "
+        gui.output_lbl.setText(output)
+
+    def fonte_b(self):
+        """ Segunda fonte das duas fontes diferentes de representação dos dígitos: 15 pontos """
+        #number0#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 0)
+        #number1#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 1)
+        #number2#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 2)
+        #number3#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 3)
+        #number4#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 4)
+        #number5#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 5)
+        #number6#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 6)
+        #number7#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 7)
+        #number8#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 8)
+        #number9#
+        self.train_one([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1], 9)
 
 if __name__ == "__main__":
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     app = QApplication([])
     gui = Gui()
+
+    rede_neural = RedeNeural()
+    rede_neural.train_all()
+    gui.train_pb.clicked.connect(rede_neural.train_current)
+    gui.run_pb.clicked.connect(rede_neural.run)
+
     gui.window.show()
     app.exec_()
